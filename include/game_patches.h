92680a8a327f9344e6df1d6052ba89c1a124115c
lunarsoap5 <40438826+lunarsoap5@users.noreply.github.com>
2020-04-22 01:28:50 -0500

    Added MS Puzzle Skip and Fixed Goats 2 Skip

--- include/game_patches.h
+++ include/game_patches.h
@@ -138,4 +138,9 @@ namespace mod::game_patch
 	 * when spawning in goats 2, will warp you to Illia taking Epona CS
 	 */
 	void skipGoats2();
+
+	/**
+	 * opens the door the the master sword in sacred grove
+	 */
+	void skipGrovePuzzle();
 }
